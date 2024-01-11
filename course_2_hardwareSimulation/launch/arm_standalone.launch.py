37af151c7dc933e363149c9fc59fbdd586434f8b
GurpartapSS <gurpartapsingh.sarkaria@gmail.com>
2024-01-11 14:09:09 -0500

    fixing interial tag for box macro which was preventing the robot arm to spawn in gazebo

--- course_2_hardwareSimulation/launch/arm_standalone.launch.py
+++ course_2_hardwareSimulation/launch/arm_standalone.launch.py
@@ -3,15 +3,13 @@ from launch_ros.actions import Node
 from launch.substitutions import Command
 from ament_index_python import get_package_share_path
 import os
-
 def generate_launch_description():
     ld = LaunchDescription()
 
     #get path to the robot URDF file
-    urdf_path = os.path.join(get_package_share_path('my_robot_description'),'urdf','standalone_arm.xacro')
+    urdf_path = os.path.join(get_package_share_path('my_robot_description'),'urdf','standalone_arm.urdf.xacro')
     rviz_path = os.path.join(get_package_share_path('my_robot_description'),'rviz','arm_standalone.rviz')
-    # robot_desc = ParameterValue(Command(['xacro ', urdf_path]),value_type=str)
-    # robot_desc = Command(['xacro ', urdf_path])
+    world_path = os.path.join(get_package_share_path('my_robot_description'),'world','my_robot.world')
 
 
     robot_publisher_node = Node(
@@ -22,10 +20,10 @@ def generate_launch_description():
             ]
     )
 
-    # joint_states_node = Node(
-    #     package="joint_state_publisher_gui",
-    #     executable="joint_state_publisher_gui"
-    # )
+    joint_states_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
 
     rviz_node = Node(
         package="rviz2",
@@ -35,17 +33,19 @@ def generate_launch_description():
 
     launch_gazeo = actions.IncludeLaunchDescription(
         launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join('/opt/ros/humble/share/gazebo_ros/launch/', 'gazebo.launch.py')))
-            # launch_arguments={'world': world_path}.items())
+            os.path.join('/opt/ros/humble/share/gazebo_ros/launch/', 'gazebo.launch.py')),
+            launch_arguments={'world': world_path}.items())
     
     spawmer_node = Node(
         package="gazebo_ros",
         executable="spawn_entity.py",
-        arguments=['-topic', '/robot_description','-entity', 'Maya']
+        arguments=['-topic', '/robot_description','-entity', 'Master', "-x", "0.0", "-y", "0.0", "-z", "0.0"],
+        output="screen"
     )
 
     ld.add_action(robot_publisher_node)
     ld.add_action(rviz_node)
+    ld.add_action(joint_states_node)
     ld.add_action(launch_gazeo)
     ld.add_action(spawmer_node)
     return ld
