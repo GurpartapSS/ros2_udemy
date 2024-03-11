02b227c1b8bf1f27b78b5d09bafb3ab57cb113ae
GurpartapSS <gurpartapsingh.sarkaria@gmail.com>
2024-03-11 01:28:05 -0400

    Adding complete Robot Server

--- course_3_action_server/activity_pkg/src/activity1_action_server.cpp
+++ course_3_action_server/activity_pkg/src/activity1_action_server.cpp
@@ -9,29 +9,54 @@ using GoalHandle = rclcpp_action::ServerGoalHandle<MoveRobo>;
 class MoveRoboServerNode : public rclcpp::Node
 {
 public:
-    MoveRoboServerNode() : Node("move_robo"),intial_position_(50)
+    MoveRoboServerNode() : Node("move_robo"),robot_position_(50)
     {
+        cb_group_=this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+
         move_robo_server_ = rclcpp_action::create_server<MoveRobo>(
             this, "move_robo",
             std::bind(&MoveRoboServerNode::goalCallBack, this, _1, _2),
             std::bind(&MoveRoboServerNode::cancelCallBack, this, _1),
             std::bind(&MoveRoboServerNode::handleGoalCallBack, this, _1),
             rcl_action_server_get_default_options(),
-            cb_group);
+            cb_group_);
         RCLCPP_INFO(this->get_logger(),"Action Server Started");
     }
 
 private:
+    std::mutex mutex_;
+    int robot_position_;
     rclcpp_action::Server<MoveRobo>::SharedPtr move_robo_server_;
-    rclcpp::CallbackGroup::SharedPtr cb_group;
-    int intial_position_;
+    rclcpp::CallbackGroup::SharedPtr cb_group_;
+    std::shared_ptr<GoalHandle> goal_handle_;
+    rclcpp_action::GoalUUID preempted_goal_id_;
 
     rclcpp_action::GoalResponse goalCallBack(
         const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const MoveRobo::Goal> goal) {
+            (void)uuid;
+        if( goal->velocity < 0) {
+            RCLCPP_INFO(this->get_logger(),"Goal Rejected");
+            return rclcpp_action::GoalResponse::REJECT;
+        }
+
+        //Policy: preempt previous goal for new one
+
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if(goal_handle_) {
+                if(goal_handle_->is_active()) {
+                    RCLCPP_INFO(this->get_logger(),"Kat Gaya! Setting new GOAL");
+                    preempted_goal_id_ = goal_handle_->get_goal_id();
+                }
+            }
+        }
+        RCLCPP_INFO(this->get_logger(),"Goal Accepted");
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
     rclcpp_action::CancelResponse cancelCallBack(const std::shared_ptr<GoalHandle> goal_handle) {
+        (void)goal_handle;
+        RCLCPP_INFO(this->get_logger(),"Goal Canceled");
         return rclcpp_action::CancelResponse::ACCEPT;
     }
 
@@ -40,15 +65,52 @@ private:
     }
 
     void execMove(const std::shared_ptr<GoalHandle> goal_handle) {
+
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            goal_handle_ = goal_handle;
+        }
+
+        // Get Goal
         int final_position = goal_handle->get_goal()->position;
         int velocity = goal_handle->get_goal()->velocity;
-        int current_position = intial_position_;
-        int distance = final_position - current_position;
-        while(final_position > current_position) {
-            if(distance < velocity) {
-                
+        // Execute
+        auto result = std::make_shared<MoveRobo::Result>();
+        auto feedback = std::make_shared<MoveRobo::Feedback>();
+
+        int distance = final_position - robot_position_;
+        while(distance != 0) {
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+                if(goal_handle->get_goal_id() == preempted_goal_id_) {
+                    result->position=robot_position_;
+                    goal_handle->abort(result);
+                    return;
+                }
             }
+            // Important else the counter will keep going until the goal is reached 
+            if(goal_handle->is_canceling()) {
+                result->position = robot_position_;
+                goal_handle->canceled(result);
+                return;
+            }
+            distance = final_position - robot_position_;
+            if(abs(distance) < velocity) {
+                RCLCPP_INFO(this->get_logger(),"last step distance: %d position: %d", distance, robot_position_);
+                velocity = abs(distance);
+            }
+            if(distance > 0) {
+                robot_position_ += velocity;
+            } else if(distance < 0) {
+                robot_position_ -= velocity;
+            }
+            feedback->current_position = robot_position_;
+            goal_handle->publish_feedback(feedback);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
+
+        result->position = robot_position_;
+        goal_handle->succeed(result);
     }
 };
 
